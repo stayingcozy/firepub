8175e189d20651623465f23a47a40fe7af5ed430
stayingcozy <cody.colclough@gmail.com>
2024-01-16 11:51:33 -0600

    adding serverIP to stream field

--- addStream.go
+++ addStream.go
@@ -7,11 +7,12 @@ import (
 	"cloud.google.com/go/firestore"
 )
 
-func addStream(ctx context.Context, client *firestore.Client, newDoc string, url string, user string, pass string) error {
+func addStream(ctx context.Context, client *firestore.Client, newDoc string, url string, user string, pass string, serverIP string) error {
 	_, err := client.Collection("streams").Doc(newDoc).Set(ctx, map[string]interface{}{
 			"url":    url,
 			"user":   user,
 			"pass":   pass,
+			"server": serverIP,
 			"status": "readyToStream",
 	})
 	if err != nil {
